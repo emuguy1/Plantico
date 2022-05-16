32d4aefcdf1b5844af7eceadb7774f50a6541f3b
Tobias Reithmaier <tobias.0104.reithmaier@gmail.com>
2022-05-16 14:06:40 +0200

    Add Searchtab with base functionality
    Added a Searchpage with a basic layout
    Added new Activity for searching
    Registered the Activity in the AndroidManifest.xml
    Navigation to the Searchpage by Clicking on the third Icon in the Navbar
    Added basic search functionality: You can search for the plantname. By default all plants are displayed. When searching all plants containing the name are displayed. When you finish the query by submitting, all fitting plants get shown. If there are no plants you'll see that there are no results for your query.

---
+++ app/src/main/java/de/othr/plantico/ui/search/SearchableActivity.kt
@@ -0,0 +1,83 @@
+package de.othr.plantico.ui.search
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ListAdapter
+import android.widget.SearchView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import de.othr.plantico.R
+import de.othr.plantico.TestViewModel
+import de.othr.plantico.TestViewModelFactory
+import de.othr.plantico.database.PlantApplication
+import de.othr.plantico.database.entities.Plant
+import de.othr.plantico.databinding.ActivitySearchBinding
+import de.othr.plantico.ui.homescreen.PlantAdapter
+
+class SearchableActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+    private val testViewModel: TestViewModel by viewModels {
+        TestViewModelFactory((application as PlantApplication).repository)
+    }
+
+    var adapter: ListAdapter? = null
+    var allPlants: List<Plant> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val adapter = PlantAdapter()
+        binding.recyclerviewSearchedPlants.adapter = adapter
+        binding.recyclerviewSearchedPlants.layoutManager = LinearLayoutManager(this)
+
+        testViewModel.allPlants.observe(this) { plants ->
+            plants.let {
+                // Set the plants list which should get searched
+                allPlants = it
+                // Set the Initial plants list in the adapter
+                // TODO: Maybe change this to frequently visited plants later on?
+                adapter.submitList(it)
+                binding.numberResultsFound.text = getString(R.string.results_found, it.size)
+                if(it.isNotEmpty()){
+                    binding.recyclerviewSearchedPlants.visibility= View.VISIBLE
+                    binding.noResultsFound.visibility= View.GONE
+                }
+            }
+        }
+
+        // Register Listener
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                val results = searchForPlantsInList(allPlants, query)
+                if(results.isEmpty()) {
+                    binding.recyclerviewSearchedPlants.visibility= View.GONE
+                    binding.noResultsFound.visibility= View.VISIBLE
+                }
+                adapter.submitList(
+                    results
+                )
+                binding.numberResultsFound.text = getString(R.string.results_found, results.size)
+                return false
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                val results = searchForPlantsInList(allPlants, newText)
+                adapter.submitList(
+                    results
+                )
+                binding.numberResultsFound.text = getString(R.string.results_found, results.size)
+                return false
+            }
+        })
+    }
+
+    //TODO: Maybe change this to Filtering in database instead of accessing all plants?
+    //Search for plants that contain the query as a substring. Not case-sensitive!
+    fun searchForPlantsInList(plants : List<Plant>, query : String) : List<Plant> {
+        return plants.filter { plant: Plant -> plant.plantName.contains(query, ignoreCase = true) }
+    }
+}

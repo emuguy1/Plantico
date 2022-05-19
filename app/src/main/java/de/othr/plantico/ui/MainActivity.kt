889306357fbcee1ad7e29f26c616378b614ba858
Armand792 <68748145+Armand792@users.noreply.github.com>
2022-05-19 12:15:02 +0200

    -created menu bar -added basic plant activity -added functionality to swap between activities

--- app/src/main/java/de/othr/plantico/ui/MainActivity.kt
+++ app/src/main/java/de/othr/plantico/ui/MainActivity.kt
@@ -3,14 +3,18 @@ package de.othr.plantico.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import de.othr.plantico.R
 import de.othr.plantico.TestViewModel
 import de.othr.plantico.TestViewModelFactory
 import de.othr.plantico.database.PlantApplication
 import de.othr.plantico.databinding.ActivityMainBinding
 import de.othr.plantico.ui.homescreen.HomescreenActivity
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,10 +36,41 @@ class MainActivity : AppCompatActivity() {
             // Update the cached copy of the words in the adapter.
             plants.let { adapter.submitList(it) }
         }
-        binding.button2.setOnClickListener {
-            val intent = Intent(this, HomescreenActivity::class.java)
-            startActivity(intent)
+
+        binding.bottomNavigation.selectedItemId = R.id.action_search
+
+        binding.bottomNavigation.setOnItemSelectedListener{ menu ->
+
+            when (menu.itemId) {
+
+                R.id.action_home -> {
+                    val intent = Intent(this, HomescreenActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.action_plant -> {
+                    val intent = Intent(this, PlantActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.action_search -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.action_user -> {
+
+                    true
+                }
+
+                else -> false
+            }
         }
+
     }
+
 
 }
--- app/src/main/java/de/othr/plantico/ui/PlantActivity.kt
+++ app/src/main/java/de/othr/plantico/ui/PlantActivity.kt
@@ -1,13 +1,16 @@
 package de.othr.plantico.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import de.othr.plantico.R
 import de.othr.plantico.TestViewModel
 import de.othr.plantico.TestViewModelFactory
 import de.othr.plantico.database.PlantApplication
 import de.othr.plantico.database.entities.Plant
 import de.othr.plantico.databinding.ActivityPlantBinding
+import de.othr.plantico.ui.homescreen.HomescreenActivity
 
 class PlantActivity : AppCompatActivity() {
     private val testViewModel: TestViewModel by viewModels {
@@ -30,7 +33,7 @@ class PlantActivity : AppCompatActivity() {
                 plantList.addAll(it)
                 val plant = plantList.find { plant -> plant.id == plantID }
                 if (plant != null) {
-                    binding.plantDetailHeader.text = plant.plantName
+                    //binding.plantDetailHeader.text = plant.plantName
                 }
             }
         }
@@ -40,8 +43,40 @@ class PlantActivity : AppCompatActivity() {
         if (bundle != null) {
             plantID = bundle.getInt(SELECTED_PLANT)
 
-
         }
+        binding.bottomNavigation.selectedItemId = R.id.action_plant
+
+        binding.bottomNavigation.setOnItemSelectedListener{ menu ->
+
+            when (menu.itemId) {
+
+                R.id.action_home -> {
+                    val intent = Intent(this, HomescreenActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.action_plant -> {
+                    val intent = Intent(this, PlantActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.action_search -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.action_user -> {
+
+                    true
+                }
+
+                else -> false
+            }
+        }
+
     }
 
     companion object {

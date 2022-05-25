f2ed076a20d6d712b614d9c0c200c71df30b44c1
Tobias Reithmaier <tobias.0104.reithmaier@gmail.com>
2022-05-25 12:21:27 +0200

    Fix Example Test, add config for running all tests

--- app/src/test/java/de/othr/plantico/ExampleUnitTest.kt
+++ app/src/test/java/de/othr/plantico/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package de.othr.plantico
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).

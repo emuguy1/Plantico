8bd25c794f5eca5a511193792f06eb29ecc812af
emuguy1 <emanuel.erben@gmail.com>
2022-05-25 14:17:50 +0200

    Added Test for add Days

--- app/src/test/java/de/othr/plantico/DateExtensionTest.kt
+++ app/src/test/java/de/othr/plantico/DateExtensionTest.kt
@@ -1,5 +1,6 @@
 package de.othr.plantico
 
+import de.othr.plantico.ui.homescreen.addDays
 import de.othr.plantico.ui.homescreen.toPlanticoString
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -8,10 +9,20 @@ import java.util.*
 
 internal class DateExtensionTest {
 
+    private val twentyfourthMay: Date = Date.from(Instant.ofEpochSecond(1653394485))
+    private val twentyfifthMay: Date = Date.from(Instant.ofEpochSecond(1653480886))
+
     @Test
     fun toPlanticoString() {
         val expected = "24th May"
-        val actual = Date.from(Instant.ofEpochSecond(1653414767)).toPlanticoString()
+        val actual = twentyfourthMay.toPlanticoString()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun addDays(){
+        val expected = twentyfifthMay
+        val actual = twentyfourthMay.addDays(1)
         assertEquals(expected, actual)
     }
 }
